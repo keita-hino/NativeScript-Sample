3746350886f87ec2aa78177f910e234c8e6990b5
keita <keitahino0415@gmail.com>
2021-02-28 13:11:04 +0900

    スタイル整えたり、タイトルの頭に絵文字を付けたり

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,16 +2,21 @@
   <Page>
     <ActionBar title="Zenn記事一覧"/>
     <GridLayout columns="*" rows="*">
-      <Button v-if="!articles" text="Button" @tap="onClick" />
+      <Button v-if="!articles" text="Zennの記事取得しまっせ！" @tap="onClick" />
+      <ActivityIndicator :busy="isLoading"/>
 
       <template v-if="articles">
         <ListView 
-          for="item in articles" 
+          for="article in articles" 
           :items="articles"
+          class="list-article"
           @itemTap="onButtonTap"
         >
           <v-template>
-            <Label :text="item.title" />
+            <Label
+              :text="titleWithEmoji(article)" 
+              class="article-title"
+            />
           </v-template>
         </ListView>
       </template>
@@ -26,12 +31,18 @@
   export default defineComponent({
     setup(){
       const articles = ref();
+      const isLoading = ref(false);
       
       const onClick = async() => {        
+        isLoading.value = true;
         // Zenn
-        const resnponse = await fetchZennArticles()
-        articles.value = resnponse.data.articles
+        const resnponse = await fetchZennArticles();
+        articles.value = resnponse.data.articles;
+
+        isLoading.value = false;
       }
+
+      const titleWithEmoji = (article) => { return `${article.emoji} ${article.title}` }
 
       const onButtonTap = () => {
 
@@ -39,6 +50,8 @@
 
       return {
         articles,
+        isLoading,
+        titleWithEmoji,
         onClick,
         onButtonTap
       }
@@ -47,15 +60,27 @@
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #ffffff;
-    }
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20;
+    color: #ffffff;
+  }
+
+  .list-article {
+    margin: 20, 15;
+    font-size: 30;
+    border-radius: 20;
+  }
+
+  .article-title {
+    font-size: 20;
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
 </style>
