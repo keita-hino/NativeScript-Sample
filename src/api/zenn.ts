84f09b67ece6cff055b23fa3eab12eb99cd58c46
keita <keitahino0415@gmail.com>
2021-03-13 11:27:12 +0900

    キーワードで検索できる機能追加

--- src/api/zenn.ts
+++ src/api/zenn.ts
@@ -1,3 +1,5 @@
 import zennAxios from '../lib/zennAxios'
 
 export const fetchZennArticles = async () => await zennAxios.get(`/articles`);
+
+export const searchZennArticles = async (searchText: string) => await zennAxios.get(`/search?q=${searchText}&source=articles&page=1`);
